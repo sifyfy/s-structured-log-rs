d8b1a7c590c5e2962152774fdab63cc09c8bc488
siphilia <siphilia.rn@gmail.com>
2016-07-01 01:19:45 +0900

    Add document files

---
+++ doc/implementors/log/trait.Log.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['s_structured_log'] = ["impl <a class='trait' href='log/trait.Log.html' title='log::Log'>Log</a> for <a class='struct' href='s_structured_log/struct.JsonLogger.html' title='s_structured_log::JsonLogger'>JsonLogger</a>",];implementors['s_structured_log'] = ["impl <a class='trait' href='log/trait.Log.html' title='log::Log'>Log</a> for <a class='struct' href='s_structured_log/struct.JsonLogger.html' title='s_structured_log::JsonLogger'>JsonLogger</a>",];implementors['s_structured_log'] = ["impl <a class='trait' href='log/trait.Log.html' title='log::Log'>Log</a> for <a class='struct' href='s_structured_log/struct.JsonLogger.html' title='s_structured_log::JsonLogger'>JsonLogger</a>",];implementors['s_structured_log'] = ["impl <a class='trait' href='log/trait.Log.html' title='log::Log'>Log</a> for <a class='struct' href='s_structured_log/struct.JsonLogger.html' title='s_structured_log::JsonLogger'>JsonLogger</a>",];implementors['s_structured_log'] = ["impl <a class='trait' href='log/trait.Log.html' title='log::Log'>Log</a> for <a class='struct' href='s_structured_log/struct.JsonLogger.html' title='s_structured_log::JsonLogger'>JsonLogger</a>",];implementors['s_structured_log'] = ["impl <a class='trait' href='log/trait.Log.html' title='log::Log'>Log</a> for <a class='struct' href='s_structured_log/struct.JsonLogger.html' title='s_structured_log::JsonLogger'>JsonLogger</a>",];implementors['s_structured_log'] = ["impl <a class='trait' href='log/trait.Log.html' title='log::Log'>Log</a> for <a class='struct' href='s_structured_log/struct.JsonLogger.html' title='s_structured_log::JsonLogger'>JsonLogger</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/s_structured_log/trait.StructuredLog.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['s_structured_log'] = [];implementors['s_structured_log'] = [];implementors['s_structured_log'] = [];implementors['s_structured_log'] = [];implementors['s_structured_log'] = [];implementors['s_structured_log'] = [];implementors['s_structured_log'] = [];implementors['s_structured_log'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
